628e9c0e94a4e50bb6501dc378802ff190947667
Maryam Hussain <maryamhussain@maryams-mbp.home>
2022-05-13 21:19:44 +0100

    render each city from local storage

--- assets/index.js
+++ assets/index.js
@@ -17,40 +17,32 @@ const readFromLocalStorage = (key, defaultValue) => {
 
   const renderRecentSearches = () => {
     const recentSearches = readFromLocalStorage("rencentSearches", []);
+    // const recentSearches =  ["London", "Tokyo", "Berlin"];
   
     if(recentSearches.length){
+
+        const createRecentCity = (city) => {
+            return `<li
+                class="btn btn-outline-info text-center mt-2 mb-2 w-1"
+                data-city="${city}"
+            >
+            ${city}
+            </li>`;
+        };
+
+        const recentCities = recentSearches.map(createRecentCity).join("");
+
+        console.log(recentCities)
       //   render recent searches list
       const ul = `<ul class="list-group p-3" id="city-list">
-      <li
-        class="btn btn-outline-info text-center mt-2 mb-2 w-1"
-        data-city="London"
-      >
-        London
-      </li>
-      <li
-        class="btn btn-outline-info text-center mt-2 mb-2 w-100"
-        data-city="Tokyo"
-      >
-        Tokyo
-      </li>
-      <li
-        class="btn btn-outline-info text-center mt-2 mb-2 w-100"
-        data-city="Berlin"
-      >
-        Berlin
-      </li>
-      <li
-        class="btn btn-outline-info text-center mt-2 mb-2 w-100"
-        id="city"
-      >
-        Birmingham
-      </li>
-    </ul>`
+         ${recentCities}
+        </ul>`;
+        recentSearchesContainer.append(ul);
     } else{
       //  show an elert
       const alert = `<div class="alert alert-warning text-center" role="alert">
           No recent searches
-      </div>`
+      </div>`;
       // append
       recentSearchesContainer.append(alert)
     }
