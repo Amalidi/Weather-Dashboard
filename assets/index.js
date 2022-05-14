133b8866bd5aeb387975cdf1f556377748f07ca3
Maryam Hussain <maryamhussain@maryams-mbp.home>
2022-05-14 21:50:33 +0100

    added dates in javascript

--- assets/index.js
+++ assets/index.js
@@ -27,17 +27,40 @@ const readFromLocalStorage = (key, defaultValue) => {
     localStorage.setItem(key, stringifiedValue);
   }; 
 
-  const renderCurrentData = () => {
+  const constructUrl = (baseUrl, params) => {
+    const queryParams = new URLSearchParams(params).toString();
+  
+    return queryParams ? `${baseUrl}?${queryParams}` : baseUrl;
+  };
+  
+  const fetchData = async (url, options = {}) => {
+    try {
+      const response = await fetch(url, options);
+  
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        throw new Error("Failed to fetch data");
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  const renderCurrentData = (data) => {
       const currentWeatherCard = `<div
         class="current-weather text-white ml-1 mr-1 mt-3 mb-3"
         id="current-weather-card"
         >
         <div class="card-header d-flex">
-            <h3 id="cityName">Name of City</h3>
-            <h3 class="px-5" id="currentdate">MM/DD/YY</h3>
+            <h3 id="cityName">${data.cityName}</h3>
+            <h3 class="px-5" id="currentdate">${moment
+            .unix(1652526000)
+            .format("dddd, Do MMM, YYYY")}</h3>
             <div class="card-box text-center">
             <img
-                src="http://openweathermap.org/img/w/04d.png"
+                src="http://openweathermap.org/img/w/${data.weatherData.current.weather[0].icon}.png"
                 alt="weather icon"
             />
             </div>
@@ -47,26 +70,26 @@ const readFromLocalStorage = (key, defaultValue) => {
             <div class="col-sm-12 col-md-4 p-2 border fw-bold">
                 Temperature
             </div>
-            <div class="col-sm-12 col-md-8 p-2 border">16&deg; C</div>
+            <div class="col-sm-12 col-md-8 p-2 border">${data.weatherData.current.temp}&deg; C</div>
             </div>
             <div class="row g-0">
             <div class="col-sm-12 col-md-4 p-2 border fw-bold">
                 Humidity
             </div>
-            <div class="col-sm-12 col-md-8 p-2 border">20&percnt;</div>
+            <div class="col-sm-12 col-md-8 p-2 border">${data.weatherData.current.humidity}&percnt;</div>
             </div>
             <div class="row g-0">
             <div class="col-sm-12 col-md-4 p-2 border fw-bold">
                 Wind Speed
             </div>
-            <div class="col-sm-12 col-md-8 p-2 border">10 MPH</div>
+            <div class="col-sm-12 col-md-8 p-2 border">${data.weatherData.current.wind_speed} MPH</div>
             </div>
             <div class="row g-0">
             <div class="col-sm-12 col-md-4 p-2 border fw-bold">
                 UV Index
             </div>
             <div class="col-sm-12 col-md-8 p-2 border">
-                <span class="bg-success text-white px-3 rounded-2">1.5</span>
+                <span class="bg-success text-white px-3 rounded-2">${data.weatherData.current.uvi}</span>
             </div>
             </div>
         </div>
@@ -194,6 +217,46 @@ const readFromLocalStorage = (key, defaultValue) => {
     }
   };
 
+  const fetchWeatherData = async (cityName) => {
+      // data from Api
+
+        // get the url
+        const currentDataUrl = constructUrl(
+            "https://api.openweathermap.org/data/2.5/weather",
+            {
+              q: cityName,
+              appid: "8109f605d79877f7488a194794a29013",
+            }
+          );
+
+          const currentData = await fetchData(currentDataUrl);
+
+          // get lat, lon and city name
+          const lat = currentData?.coord?.lat;
+          const lon = currentData?.coord?.lon;
+          const displayCityName = currentData?.name;
+
+        //   forcast url
+
+          const forecastDataUrl = constructUrl(
+            "https://api.openweathermap.org/data/2.5/onecall",
+            {
+              lat: lat,
+              lon: lon,
+              exclude: "minutely,hourly",
+              units: "metric",
+              appid: "8109f605d79877f7488a194794a29013",
+            }
+          );
+
+          const forecastData = await fetchData(forecastDataUrl);
+
+          return {
+            cityName: displayCityName,
+            weatherData: forecastData,
+          };
+  };
+
 const handleRecentSearchClick = (event) => {
     const target = $(event.target)
     // check if the target is a list item 
@@ -204,33 +267,7 @@ const handleRecentSearchClick = (event) => {
     }
 };
 
-// const handleFormSubmit = (event) => {
-//     event.preventDefault();
-
-//     // form input
-//     const cityName = $("#search-input").val();
-
-//     // validate
-//     if(cityName){
-
-//         const recentSearches = readFromLocalStorage("recentSearches", []);
-
-//         // city name into an array
-//         recentSearches.push(cityName);
-
-//         // write recent searches to LS
-//         writeToLocalStorage("recentSearches", recentSearches);
-
-//         // remove previous items
-//         recentSearchesContainer.children().last().remove();
-
-//         // re-render recent cities
-//         renderRecentSearches();
-
-//     }
-// };
-
-const handleFormSubmit = (event) => {
+const handleFormSubmit = async (event) => {
     event.preventDefault();
   
     // get form input value
@@ -238,13 +275,15 @@ const handleFormSubmit = (event) => {
   
     // validate
     if (cityName) {
-        // data from Api
+        // fetch weather data
+        const weatherData = await fetchWeatherData(cityName);
+
 
         // render current date
-        renderCurrentData();
+        renderCurrentData(weatherData);
 
         // render forcast data
-        renderForecastData();
+        renderForecastData(weatherData);
 
         console.log(cityName)
       // get recentSearches from LS
